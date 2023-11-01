12b6958746da07c8e28030dd24c0c1f07ee5e5d8
SatsForFreedom <SatoshyBoy@proton.me>
2023-11-01 16:08:36 -0400

    add custom settings for PCB 2.A

--- main/adc.c
+++ main/adc.c
@@ -4,10 +4,16 @@
 // static const char *TAG = "adc.c";
 static esp_adc_cal_characteristics_t adc1_chars;
 
+#if CONFIG_BITAXE2_A
+    #define VCORE_ADC ADC1_CHANNEL_3
+#else
+    #define VCORE_ADC ADC1_CHANNEL_1
+#endif
+
 // Sets up the ADC to read Vcore. Run this before ADC_get_vcore()
 void ADC_init(void)
 {
-    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(VCORE_ADC, ADC_ATTEN_DB_11);
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
 }
 
@@ -15,5 +21,5 @@ void ADC_init(void)
 uint16_t ADC_get_vcore(void)
 {
     adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
-    return esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC1_CHANNEL_1), &adc1_chars);
+    return esp_adc_cal_raw_to_voltage(adc1_get_raw(VCORE_ADC), &adc1_chars);
 }
